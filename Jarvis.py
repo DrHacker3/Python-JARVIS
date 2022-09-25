ebc9df014d73f4ea3e7415fb8a3ca5fee298124a
Vipul <vipulraj011@gmail.com>
2022-09-25 11:14:37 +0530

    added weather checker according to city

--- Jarvis.py
+++ Jarvis.py
@@ -2,6 +2,8 @@ import time
 import os 
 from plyer import notification
 from AppOpener import run 
+from bs4 import BeautifulSoup
+import requests
 
 os.system("color 0c")
 print('''
@@ -128,6 +130,33 @@ while 1:
                 run(Y)
             else:
                 print("Please check the application name you have typed")
+                
+                
+        elif "weather" in A:
+            headers = {
+             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+
+            def weather(city):
+             city = city.replace(" ", "+")
+             res = requests.get(
+              f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
+             print("Searching...\n")
+             soup = BeautifulSoup(res.text, 'html.parser')
+             location = soup.select('#wob_loc')[0].getText().strip()
+             time = soup.select('#wob_dts')[0].getText().strip()
+             info = soup.select('#wob_dc')[0].getText().strip()
+             weather = soup.select('#wob_tm')[0].getText().strip()
+             print(location)
+             print(time)
+             print(info)
+             print(weather+"°C")
+
+
+            city = input("Enter the Name of City -> ")
+            city = city+" weather"
+            weather(city)
+            print("Have a Nice Day:)")
             
     else:
         print("I'm not sure I understand", C)
